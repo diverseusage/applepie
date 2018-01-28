94808c7db2790b522739e9ea35c57a9731a5e112
diverseusage <diverseusage@gmail.com>
2018-01-28 21:43:56 +0800

    i

--- source/cyberbot/telebot.py
+++ source/cyberbot/telebot.py
@@ -10,14 +10,14 @@ class Telebot:
         self.token = bot_token
 
     START_MESSAGE = """Hello {}
-        Please enter the singer followed by the title of the song. 
-        e.g. the chainsmokers closer
-        To get your youtube playlist, 
-        1. Add @youtube
-        2. Sign in to your youtube account by entering /auth 
-        3. Enter /setting 
-        4. Toggle to 'Suggest liked video' button
-        5. Click 'Save & go to chat' button """
+    Please enter the singer followed by the title of the song. 
+    e.g. the chainsmokers closer
+    To get your youtube playlist, 
+    1. Add @youtube
+    2. Sign in to your youtube account by entering /auth 
+    3. Enter /setting 
+    4. Toggle to 'Suggest liked video' button
+    5. Click 'Save & go to chat' button """
 
     # def button(self, bot, update):
     #     query = update.callback_query
@@ -25,23 +25,31 @@ class Telebot:
     #                         chat_id=query.message.chat_id,
     #                         message_id=query.message.message_id)
 
-    def start(self, bot, update):
+    def command_start(self, bot, update):
         keyboard = [[InlineKeyboardButton("Top Hits", callback_data='Top Hits')],
                     [InlineKeyboardButton("Top Artists", callback_data='Top Artists')]]
         reply_markup = InlineKeyboardMarkup(keyboard)
         update.message.reply_text(self.START_MESSAGE.format(update.message.from_user.first_name), reply_markup=reply_markup)
 
     def display(self, bot, update):
-        lyrics_manager = LyricsManager()
         message = update.message.text
+        lyrics_manager = LyricsManager()
         update.message.reply_text(lyrics_manager.display_lyrics(message))
+
+    def test_call(self, bot, update):
+        print(update)
 
     def run_update(self):
         updater = Updater(self.token)
 
-        updater.dispatcher.add_handler(CommandHandler('start', self.start))
+        # Command Handlers
+        updater.dispatcher.add_handler(CommandHandler('start', self.command_start))
 
+        # Message Handler
         updater.dispatcher.add_handler(MessageHandler(Filters.text, self.display))
+        
+        # Callback Handler
+        updater.dispatcher.add_handler(CallbackQueryHandler(self.test_call))
 
         updater.start_polling()
         updater.idle()
